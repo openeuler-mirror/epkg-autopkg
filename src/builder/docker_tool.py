0a89129d3753171a0f0a86326deec8a5b90d0474
qiu-tangke <q_qiutangke@163.com>
2024-08-26 09:38:29 +0800

    change shell method

--- src/builder/docker_tool.py
+++ src/builder/docker_tool.py
@@ -1,8 +1,10 @@
 import os
 import subprocess
-import src.builder
+import yaml
 from src.log import logger
+from src.builder import scripts_path
 from src.config.config import configuration
+from src.config.yamls import yaml_path
 
 
 def get_docker_container(name="autopkg_build"):
@@ -19,8 +21,9 @@ def get_docker_container(name="autopkg_build"):
 
 
 def run_docker_script(build_system):
-    docker_run_path = os.path.join(src.builder.scripts_path, "docker_build.sh")
-    cmd = f"{docker_run_path} -b {build_system} -d {configuration.download_path} -s {src.builder.scripts_path}"
+    args_value = parse_yaml_args(build_system)
+    docker_run_path = os.path.join(scripts_path, "docker_build.sh")
+    cmd = f"{docker_run_path} -b {build_system} -d {configuration.download_path} -s {scripts_path} {args_value}"
     result = os.popen(cmd).read()
     logger.info(result)
     return result
@@ -28,3 +31,21 @@ def run_docker_script(build_system):
 
 def run_docker_epkg():
     pass
+
+
+def parse_yaml_args(build_system):
+    basic_yaml = os.path.join(scripts_path, "package.yaml")
+    build_system_yaml = os.path.join(yaml_path, f"{build_system}.yaml")
+    info = yaml.safe_load(basic_yaml)
+    if os.path.exists(build_system_yaml):
+        info.update(yaml.safe_load(build_system_yaml))
+    args = []
+    if "makeFlags" in info:
+        args.append("makeFlags=" + info["makeFlags"].strip())
+    if "cmakeFlags" in info:
+        args.append("cmakeFlags=" + info["cmakeFlags"].strip())
+    if "configureFlags" in info:
+        args.append("configureFlags=" + info["configureFlags"].strip())
+    if "buildRequires" in info:
+        args.append("build_requires=" + " ".join(info["buildRequires"]))
+    return " ".join(args)
--- src/builder/generic-build.sh
+++ src/builder/generic-build.sh
@@ -12,21 +12,8 @@ build_system="$1"
 # Source the required scripts
 source /root/skel.sh
 source /root/"$build_system".sh
-build_requires=`cat /root/package.yaml |shyaml get-value build_requires |sed 's/^[ \t-]*//'`
-if [ "${#build_requires}" -ne 0 ]; then
-    IFS=$'\n' read -rd '' -a packages <<<"$build_requires"
-    yum install -y ${packages[*]}
-fi
-makeFlags=`cat /root/package.yaml |shyaml get-value makeFlags`
-cmakeFlags=`cat /root/package.yaml |shyaml get-value cmakeFlags`
-configureFlags=`cat /root/package.yaml |shyaml get-value configureFlags`
-
-# Prepare the build environment
-prep
-
-# Build and install
-build
-install
+source /root/params_parser.sh
+source /root/phase.sh
 
 # Check if the build was successful
 if [ $? -eq 0 ]; then
