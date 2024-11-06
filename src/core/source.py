f0943fc77a1aadb785eb28aa49259756a1d3a2af
qiu-tangke <q_qiutangke@163.com>
2024-11-06 10:42:35 +0800

    fix bug for maven

--- src/core/source.py
+++ src/core/source.py
@@ -6,7 +6,7 @@ class Source(object):
         self.release = 0
         self.version = ""
         self.language = ""
-        # self.group = ""
+        self.group = ""
         self.files = []
 
 
